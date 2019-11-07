022772874a8d2ea01418f6075d6514c43a22606e
zhuzheng <zhuzheng@startdt.com>
2019-11-07 17:54:11 +0800

    'feat:完善路由'

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,11 +17,113 @@ const routes = [
         path: '/home',
         name: 'home',
         component: Home
+      }
+    ]
+  },
+  {
+    path: '/asset-management', // 资产管理
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'assetInfo',
+        name: 'assetInfo',
+        component: Home
       },
       {
-        path: '/about',
-        name: 'about',
-        component: () => import(/* webpackChunkName: "about" */ '../pages/Home/index.vue')
+        path: 'retrieval',
+        name: 'retrieval',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/attract-investment', // 招商管理
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'intention',
+        name: 'intention',
+        component: Home
+      },
+      {
+        path: 'task',
+        name: 'task',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/contract', // 合同管理
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'template',
+        name: 'template',
+        component: Home
+      },
+      {
+        path: 'retrieval',
+        name: 'retrieval',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/entry', // 企业进驻
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'apply',
+        name: 'apply',
+        component: Home
+      },
+      {
+        path: 'approval',
+        name: 'approval',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/property', // 物业服务
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'parking',
+        name: 'parking',
+        component: Home
+      },
+      {
+        path: 'repair',
+        name: 'repair',
+        component: Home
+      },
+      {
+        path: 'processing',
+        name: 'processing',
+        component: Home
+      }
+    ]
+  },
+  {
+    path: '/xxxx', // xxxx
+    redirect: '',
+    component: BasicLayout,
+    children: [
+      {
+        path: 'xxxx1',
+        name: 'xxxx1',
+        component: Home
+      },
+      {
+        path: 'xxxx2',
+        name: 'task',
+        component: Home
       }
     ]
   }
